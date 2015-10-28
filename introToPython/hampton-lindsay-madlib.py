7c36d4f011d4653654977830e53b218bb6787c36
Lindsay Hampton <lindsayhampton@Lindsays-MacBook-Pro-2.local>
2015-10-27 22:09:48 -0400

    Created Mad Lib & Posted Inputs

---
+++ introToPython/hampton-lindsay-madlib.py
@@ -0,0 +1,17 @@
+# MadLib Lab 1
+# By Lindsay Hampton
+
+# Inputs for our Mad Lib:
+year = raw_input("Enter a year: ")
+number = raw_input("Enter a number: ")
+funnyWord = raw_input("Enter a funny word: ")
+animal1 = raw_input("Enter an animal: ")
+instrument = raw_input("Enter an instrument: ")
+animal2 = raw_input("Enter a new animal: ")
+verb1 = raw_input("Enter a verb: ")
+object1 = raw_input("Enter an object: ")
+adjective = raw_input("Enter an adjective: ")
+animal3 = raw_input("Enter a new animal: ")
+verb2 = raw_input("Enter a new verb: ")
+object2 = raw_input("Enter a new object: ")
+object3 = raw_input("Enter a new object: ")
