dcd4b3f97738e024cb3c522f6ace341009d6fe64
Lindsay Hampton <lindsayhampton@Lindsays-MacBook-Pro-2.local>
2015-10-29 15:30:36 -0400

    Edited word choices and added Mad Lib line 2

--- introToPython/hampton-lindsay-madlib.py
+++ introToPython/hampton-lindsay-madlib.py
@@ -8,11 +8,11 @@ funnyWord = raw_input("Enter a funny word: ")
 animal1 = raw_input("Enter an animal: ")
 instrument = raw_input("Enter an instrument: ")
 animal2 = raw_input("Enter a new animal: ")
-verb1 = raw_input("Enter a verb: ")
+verb1 = raw_input("Enter a past-tense verb: ")
 object1 = raw_input("Enter an object: ")
 adjective = raw_input("Enter an adjective: ")
 animal3 = raw_input("Enter a new animal: ")
-verb2 = raw_input("Enter a new verb: ")
+verb2 = raw_input("Enter a new past-tense verb: ")
 object2 = raw_input("Enter a new object: ")
 object3 = raw_input("Enter a new object: ")
 
@@ -22,10 +22,10 @@ animal = [animal1, animal2, animal3]
 object = [object1, object2, object3]
 verb = [verb1, verb2]
 
-# # Function to combine funny words as a string
-# def diddle(funnyWord):
-#     diddle = str(funnyWord + " " + funnyWord)
-#     print diddle
+# Function to combine funny words
+def diddle(funny):
+    diddle = funny + " " + funny
+    print diddle
 
 # Stringify the year
 age = str(2015 - int(year))
@@ -33,8 +33,9 @@ age = str(2015 - int(year))
 # The Mad Lib Itself
 
 madLib1 = "When I was "+age+" years old, I had "+number+" favorite nursery rhymes. But I've got to say, my favorite " \
-        "nursery rhyme of all time was called 'Hey "+ funnyWord + funnyWord +"'. It went something like this:"
-
+        "nursery rhyme of all time was called Hey "+ funnyWord + " " + funnyWord +" and it went something like this: "
+madLib2 = "Hey "+ funnyWord + " " + funnyWord + ", the " + animal[0] + " and the " + instrument + ","
+madLib3 = "The "+ animal[1] + ""
 
 
 # Conditional Loop w/ Logical Operator
@@ -44,5 +45,6 @@ else:
      print madLib1
 
 # Loop to show user the words they chose
+print "These are the answers that you chose: "
 for a in answers:
     print "You chose the word " + a + "."
