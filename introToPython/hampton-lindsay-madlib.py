e9d8467cee19eb8ecc6baa6951bc7d6400edf89e
Lindsay Hampton <lindsayhampton@Lindsays-MacBook-Pro-2.local>
2015-10-29 14:53:54 -0400

    Set up answers array for loop

--- introToPython/hampton-lindsay-madlib.py
+++ introToPython/hampton-lindsay-madlib.py
@@ -17,6 +17,7 @@ object2 = raw_input("Enter a new object: ")
 object3 = raw_input("Enter a new object: ")
 
 # Created arrays to organize similar information
+answers = [year, number, funnyWord, animal1, instrument, animal2, verb1, object1, adjective, animal3, verb2, object2, object3]
 animal = [animal1, animal2, animal3]
 object = [object1, object2, object3]
 verb = [verb1, verb2]
