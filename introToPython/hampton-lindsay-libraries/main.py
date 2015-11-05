416b8e86d60cdeeeac4b21c67a3fe000bfa3e1cb
Lindsay Hampton <lindsayhampton@Lindsays-MacBook-Pro-2.local>
2015-11-05 14:52:31 -0500

    Set up MainHandler class

--- introToPython/hampton-lindsay-libraries/main.py
+++ introToPython/hampton-lindsay-libraries/main.py
@@ -2,3 +2,17 @@ __author__ = 'lindsayhampton'
 
 # Import the Library file so as to access its files
 import library
+
+class MainHandler:
+    # I know you said to always use underscores in naming conventions, but the assignment clearly states to use this name.
+
+    def __init__(self):
+        # Declare the attributes with classes from library.py
+        self.library(library.Library)
+        self.printer(library.Printer)
+
+        # Declare variable based on User's Input
+        # int() is required to be capable of calculating inputted information
+        fahrenheit = int(raw_input("Enter a temperature in Fahrenheit:"))
+        mph = int(raw_input("Enter a speed in Miles Per Hour(MPH):"))
+        feet = int(raw_input("Enter a length in Feet:"))
