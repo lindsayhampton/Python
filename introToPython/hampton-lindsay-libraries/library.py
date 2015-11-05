3d0ebc55d8ab800c3371898ee6f1ac08cc571a74
Lindsay Hampton <lindsayhampton@Lindsays-MacBook-Pro-2.local>
2015-11-05 12:17:12 -0500

    Declared Variables

--- introToPython/hampton-lindsay-libraries/library.py
+++ introToPython/hampton-lindsay-libraries/library.py
@@ -4,6 +4,13 @@ class Printer():
     # Constructor Function
     def __init__(self):
 
+        # Declare variables for future use
+        self.fahrenheit = 0
+        self.celsius = 0
+
+        self.mph = 0
+        self.kph = 0
+
 class Library():
     # Constructor Function
     def __init__(self):
